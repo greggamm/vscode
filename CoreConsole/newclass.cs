8af4bd378af750f0a717a00075aa9f5bf932fa95
greggamm <greg.gamm@gmail.com>
2019-01-30 11:45:05 -0800

    init1

---
+++ CoreConsole/newclass.cs
@@ -0,0 +1,12 @@
+namespace CoreConsole
+{
+    public static class newclass
+    {
+        private static string test = "sad";
+        
+        public static string get()
+        {
+            return test;
+        }
+    }
+}
